b71107f67742473ab3143fc534f3240f8ef0405c
Aizawa Takashi <aizawatakashi@Aizawa-no-MacBook-Pro.local>
2015-04-21 08:44:56 +0900

    Initial Commit

---
+++ SwiftSQLite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSQLite
+//
+//  Created by Aizawa Takashi on 2015/04/21.
+//  Copyright (c) 2015年 Aizawa Takashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
